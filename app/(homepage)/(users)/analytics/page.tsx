3935acca6b58aa9da30530502cac245471c8885e
revi aldy <revialdy.arifs@gmail.com>
2025-03-11 15:16:13 +0700

    dynamic chart

--- app/(homepage)/(users)/analytics/page.tsx
+++ app/(homepage)/(users)/analytics/page.tsx
@@ -3,7 +3,7 @@ import AnalyticsChart from '@/chart'
 
 export default function AnalyticsPage() {
   return (
-    <div className='container mx-auto flex flex-col px-14 py-10 items-center'>
+    <div className='mx-auto flex flex-col px-6 py-10 items-center'>
         <div>
             <AnalyticsChart />
         </div>
